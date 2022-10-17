ab9fd52fbc4bc21f80ad5f5eba626e0e3f0232a2
何奥 <1848852183@qq.com>
2022-10-17 20:14:29 +0800

    父子传值

---
+++ src/components/About.vue
@@ -0,0 +1,35 @@
+<template>
+    <hr>
+  <h2>About组件</h2>
+  <h3>父组件传来的值,姓名:{{ren.name}} 年龄:{{ren.age}}</h3>
+    <button @click="send">点击给父组件传值</button>
+  
+</template>
+
+<script>
+
+
+export default {
+ name : 'About',
+ props: {
+    ren: {
+        require: true,
+        type : Object
+    }
+ },
+ data(){
+    return {
+        sonName : 'son的信息'
+    }
+ },
+ methods: {
+    send(){
+        this.$emit('onSon',this.sonName)
+    }
+ }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Show.vue
+++ src/components/Show.vue
@@ -1,13 +1,36 @@
 <template>
- <h2>home组件</h2>
+ <h2>Show组件</h2>
+ <h3>子传来的信息:{{sonName}}</h3>
+ <div>
+    <About :ren="person" @onSon="showSon"></About>
+ </div>
  
 </template>
 
 <script>
-
+import About from '@/components/About'
 
 export default {
- name:'Home',
+ emits:['onSon'],
+ name:'Show',
+ components: {
+ About,
+ },
+ data(){
+ return {
+    person: {
+        name:'张三',
+        age : 18
+    },
+    sonName:'还未传值'
+ }
+ },
+ methods: {
+    showSon(e){
+        console.log(e);
+        this.sonName = e
+    }
+ },
  setup(){
  
  }
@@ -15,5 +38,5 @@ export default {
 </script>
 
 <style>
-
+ 
 </style>
