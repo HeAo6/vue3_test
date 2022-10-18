b5df86b4ae0cc8c302dfef70bbded0b979a469a7
何奥 <1848852183@qq.com>
2022-10-18 11:05:42 +0800

    作用于插槽的应用

---
+++ src/components/Child.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    Child
+    <hr>
+    <template v-for="(item,index) in names" :key="item">
+        <slot :Name="item" :index="index"> </slot>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Child',
+//接受父组件传入的值，再通过给插槽通过v-bind向上传递给父组件
+props:{
+    names: {
+        type: Array,
+        default: () => [] 
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Show.vue
+++ src/components/Show.vue
@@ -2,14 +2,30 @@
   <div>
     <FatherSlot></FatherSlot>
   </div>
+  <div>
+    <hr>
+    作用域插槽
+    <Child :names="names">
+      <template v-slot="slotProps">
+        <button>{{slotProps.Name}}---{{slotProps.index}}</button>
+      </template>
+    </Child>
+  </div>
 </template>
 
 <script>
 import FatherSlot from './FatherSlot.vue'
+import Child from './Child.vue'
 export default {
 name:'Show',
 components: {
-  FatherSlot
+  FatherSlot,
+  Child
+},
+data(){
+  return {
+    names:['why','tom','jeery','john']
+  }
 }
 }
 </script>
